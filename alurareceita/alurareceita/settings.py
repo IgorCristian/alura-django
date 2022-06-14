a8343a16fbdb21e0380872076b9bee046b7c90a9
Igor Cristian <100322776+IgorCristian@users.noreply.github.com>
2022-06-14 12:32:43 -0300

    Finalizando o segundo curso de Django

--- alurareceita/alurareceita/settings.py
+++ alurareceita/alurareceita/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pessoas',
     'receitas',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -126,3 +127,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIR = {
     os.path.join(BASE_DIR,'alurareceita/static')
 }
+
+
+# Media
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
